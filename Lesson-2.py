7d74be9382926c187b2332de6bbc17891db50c24
shanto das <114013679+Shanto-123@users.noreply.github.com>
2024-09-25 02:09:00 +0600

    Add files via upload

---
+++ Lesson-2.py
@@ -0,0 +1,17 @@
+product_name = "cofee"
+product_price = 275.67
+exp_date = "10 september 2025"
+any_discount = True
+print( product_name)
+print(product_price)
+print( exp_date)
+print(any_discount) 
+
+
+
+
+
+
+
+
+
